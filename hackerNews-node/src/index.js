176df76dad852df68943a8a9ec636c163c9fed2a
“itismadhavan” <madhavannagarajan@outlook.com>
2020-05-09 08:49:37 +0530

    a simple mutation

--- hackerNews-node/src/index.js
+++ hackerNews-node/src/index.js
@@ -1,38 +1,56 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query{
-    info : String!
-    feed : [Link!]!
-}
-type Link{
-    id: ID!
-    description: String!
-    url: String!
-}
-`
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+let idCount = links.length;
+
 const resolvers = {
-    Query: {
-        info: () => "This is an api for hackernews clone",
-        feed: () => links
+  Query: {
+    info: () => "This is an api for hackernews clone",
+    feed: () => links,
+    Link: (parent, args) => {
+      return links.filter((x) => x.id === args.id)[0];
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url
-    }
-}
+  },
+  //   Link: {
+  //     id: (parent) => parent.id,
+  //     description: (parent) => parent.description,
+  //     url: (parent) => parent.url,
+  //   },
+
+  Mutation: {
+    post: (parent, args) => {
+      let link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+
+    update: (parent, args) => {
+      links.forEach((x) => {
+        if (x.id == args.id) {
+          x.url = args.url;
+        }
+      });
+      return links.filter((x) => x.id === args.id)[0];
+    },
+  },
+};
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
-})
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+});
 
 server.start(() => {
-    console.log("server is listening at port 4000")
-})
+  console.log("server is listening at port 4000");
+});
